b2da84a460b03ee3750192530bfc423f139156c2
André Lucas <andre@ae-35.com>
2024-02-07 15:52:36 +0000

    Support building dpkg

--- tools/source-build.sh
+++ tools/source-build.sh
@@ -5,20 +5,24 @@ function usage() {
     exit 1
 }
 
+debbuild=0
 releasetype=RelWithDebInfo
 
 declare -a cmake_opts
 cmake_opts=()
 
-while getopts "c:t:" o; do
+while getopts "c:dt:" o; do
     case "${o}" in
-        t)
-            BUILD_TYPE=$OPTARG
-            echo "Setting build type '$BUILD_TYPE'"
-            ;;
         c)
             # shellcheck disable=SC2206
             cmake_opts+=("$OPTARG")
+            ;;
+        d)
+            debbuild=1
+            ;;
+        t)
+            BUILD_TYPE=$OPTARG
+            echo "Setting build type '$BUILD_TYPE'"
             ;;
         *)
             usage
@@ -39,18 +43,30 @@ export NINJA_STATUS="[%p :: t=%t/f=%f/r=%r :: %e] "
 BUILD_NPROC="${BUILD_NPROC:-$(nproc)}"
 export BUILD_NPROC
 
-export BUILD_DIR=build.$BUILD_TYPE
+function srcbuild() {
+    export BUILD_DIR=build.$BUILD_TYPE
 
-rm -rf "$BUILD_DIR"
+    rm -rf "$BUILD_DIR"
 
-set -x
-./do_cmake.sh -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
-    -DCMAKE_BUILD_TYPE="$releasetype" \
-    -DALLOCATOR=tcmalloc \
-    -DBOOST_J="$BUILD_NPROC" \
-    -DWITH_CCACHE=ON \
-    "${cmake_opts[@]}"
-cd "$BUILD_DIR"
+    set -x
+    ./do_cmake.sh -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+        -DCMAKE_BUILD_TYPE="$releasetype" \
+        -DALLOCATOR=tcmalloc \
+        -DBOOST_J="$BUILD_NPROC" \
+        -DWITH_CCACHE=ON \
+        "${cmake_opts[@]}"
+    cd "$BUILD_DIR"
 
-# Run Ninja with whatever parameters are passed to this script.
-ninja "$@"
+    # Run Ninja with whatever parameters are passed to this script.
+    ninja
+}
+
+function debbuild() {
+    ./make-debs.sh
+}
+
+if [[ $debbuild -eq 1 ]]; then
+    debbuild
+else
+    srcbuild
+fi
