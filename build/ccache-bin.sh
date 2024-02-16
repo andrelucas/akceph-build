e020472ef2524805f0c1c6287a3f86d513fc721f
André Lucas <andre@ae-35.com>
2024-02-16 16:21:00 +0000

    Unbreak ccache binary d/l

--- build/ccache-bin.sh
+++ build/ccache-bin.sh
@@ -27,9 +27,9 @@ URL="https://github.com/ccache/ccache/releases/download/v4.9.1/${TARBALL}"
 DL="${tmpdir}/${TARBALL}"
 curl -L -o "$DL" "${URL}"
 curl -L -o "${DL}.asc" "${URL}.asc"
-gpg --recv-keys "$AKCEPH_CCACHE_GPGKEYID"
+gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys "$AKCEPH_CCACHE_GPGKEYID"
 gpg --verify "${DL}.asc" "$DL"
 
 tar -C "$tmpdir" -xJf "$DL"
-cp "$tmpdir/ccache-${CCACHE_VERSION}-linux-x86_64/bin/ccache" /usr/local/bin/
+install -m 0755 -o root -g root "$tmpdir/ccache-${CCACHE_VERSION}-linux-x86_64/ccache" /usr/local/bin/
 /usr/local/bin/ccache --version
