ddbac10ffb44c5dda3083ad164f368afa1dbaa8d
André Lucas <andre@ae-35.com>
2024-02-23 18:21:40 +0000

    Add clean.sh.

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+uid="$(id -u)"
+
+sudo=""
+if [[ $uid -ne 0 ]]; then
+    sudo="sudo"
+    echo "Will use sudo(1), may prompt for password"
+fi
+
+set -e -x
+$sudo rm -rf tmp.*
+$sudo rm -rf preinstall*
+$sudo rm -rf release_*
+$sudo find release -mindepth 1 -delete
+git co release/
